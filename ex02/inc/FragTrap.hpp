9813edb0c009c61899dfae773942ef6f6ab7b3f1
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-05-02 21:15:19 +0100

    Added constructor to respect hortodox form on FragTrap

--- ex02/inc/FragTrap.hpp
+++ ex02/inc/FragTrap.hpp
@@ -5,8 +5,12 @@
 
 class FragTrap : public ClapTrap
 {
+private:
+	FragTrap();
 public:
 	FragTrap(std::string name);
+	FragTrap(const FragTrap& other);
+	FragTrap& operator=(const FragTrap& other);
 	~FragTrap();
 
 	void highFivesGuys(void);
--- ex02/src/FragTrap.cpp
+++ ex02/src/FragTrap.cpp
@@ -2,6 +2,11 @@
 
 #include "FragTrap.hpp"
 
+FragTrap::FragTrap() : ClapTrap()
+{
+	std::cout << "FragTrap created with default." << std::endl;
+}
+
 FragTrap::FragTrap(std::string name) : ClapTrap(name)
 {
 	this->_hp = 100;
@@ -10,9 +15,25 @@ FragTrap::FragTrap(std::string name) : ClapTrap(name)
 	std::cout << "FragTrap " << name << " started." << std::endl;
 }
 
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other)
+{
+	std::cout << "FragTrap " << other._name << " copied." << std::endl;
+}
+
 FragTrap::~FragTrap()
 {
 	std::cout << "FragTrap " << this->_name << " stopped." << std::endl;
+}
+
+FragTrap&
+FragTrap::operator=(const FragTrap& other)
+{
+	std::cout << "FragTrap " << this->_name << " assigned." << std::endl;
+	this->_name = other._name;
+	this->_hp = other._hp;
+	this->_ep = other._ep;
+	this->_ad = other._ad;
+	return *this;
 }
 
 void

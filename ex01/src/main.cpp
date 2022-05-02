faa71435e0cb7c198a554ba79be9bfdd6738dad1
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-05-02 19:18:39 +0100

    Added ex01

---
+++ ex01/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int
+main()
+{
+	ClapTrap a ("Clap1");
+
+	a.attack("TrapClap");
+	a.takeDamage(5);
+	a.beRepaired(5);
+
+	std::cout << std::endl;
+
+	ScavTrap b ("Scav1");
+	b.attack("TrapClap");
+	b.takeDamage(5);
+	b.beRepaired(5);
+	b.guardGate();
+	return 0;
+}

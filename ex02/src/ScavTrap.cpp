283e2acf669921db3f9e8cc280d06d15b8a61ae8
Miguel Gueifao <miguel.gueifao@gmail.com>
2022-05-02 21:06:24 +0100

    Added constructor to respect hortodox form on ScavTrap

--- ex02/src/ScavTrap.cpp
+++ ex02/src/ScavTrap.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "ScavTrap.hpp"
 
+ScavTrap::ScavTrap() : ClapTrap()
+{
+	std::cout << "ScavTrap created with default." << std::endl;
+}
+
 ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
 {
 	this->_hp = 100;
@@ -9,9 +14,25 @@ ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
 	std::cout << "ScavTrap " << name << " initialized." << std::endl;
 }
 
+ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other)
+{
+	std::cout << "ScavTrap " << other._name << " copied." << std::endl;
+}
+
 ScavTrap::~ScavTrap()
 {
 	std::cout << "ScavTrap " << this->_name << " removed." << std::endl;
+}
+
+ScavTrap&
+ScavTrap::operator=(const ScavTrap& other)
+{
+	std::cout << "ScavTrap " << this->_name << " assigned." << std::endl;
+	this->_name = other._name;
+	this->_hp = other._hp;
+	this->_ep = other._ep;
+	this->_ad = other._ad;
+	return *this;
 }
 
 void
